db5b1916374ba7e122d40279b4609795e905a53d
srishti-maurya <srishti22maurya@gmail.com>
2022-09-29 18:11:18 +0530

    fix: key in map

--- src/frontend/Pages/Playlist/Playlist.jsx
+++ src/frontend/Pages/Playlist/Playlist.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { useData } from "../../Contexts/data-context";
-import { getThumbnail } from "../../Utils/getThumbnail";
-import { MdPlaylistPlay } from "react-icons/md";
-import { Loader } from "../../Components";
+import React, { useEffect } from 'react';
+import { useData } from '../../Contexts/data-context';
+import { getThumbnail } from '../../Utils/getThumbnail';
+import { MdPlaylistPlay } from 'react-icons/md';
+import { Loader } from '../../Components';
 
 export function PlaylistVideoCard({ video, playlist }) {
   return (
@@ -15,7 +15,7 @@ export function PlaylistVideoCard({ video, playlist }) {
         <div
           style={{
             height: 140,
-            backgroundColor: "black",
+            backgroundColor: 'black',
           }}
           className="playlist-thumbnail-img"
         ></div>
@@ -43,14 +43,12 @@ export function Playlist() {
         <h3 className="margin-auto color-text-grey">No playlist found.</h3>
       ) : (
         <div className="playlist-container">
-          {state.playlist.map((singlePlaylist) => (
-            <>
-              <PlaylistVideoCard
-                playlist={singlePlaylist}
-                video={singlePlaylist.videos[0]}
-                key={singlePlaylist._id}
-              />
-            </>
+          {state.playlist.map((singlePlaylist, index) => (
+            <PlaylistVideoCard
+              playlist={singlePlaylist}
+              video={singlePlaylist.videos[0]}
+              key={index}
+            />
           ))}
         </div>
       )}
